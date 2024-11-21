964a27f60afbae038dd43426ec094a5a850717a2
Debbie <deborahhabtom7@gmail.com>
2024-11-21 14:49:22 +0200

    refrence added

--- types.tsx
+++ types.tsx
@@ -1,15 +1,3 @@
-// Code Attribution-->
-// TITLE: MAST5112/d/p/w MODULE MANUAL/GUIDE 2024-->
-// Author: The Independent Institute of Education (Pty) Ltd 2024-->
-// Date: 2024 -->
-// Version: (First Edition: 2012)-->
-// Avialabe:(https://advtechonline.sharepoint.com/:w:/r/sites/TertiaryStudents/_layouts/15/Doc.aspx?sourcedoc=%7B8B4938D0-6B6B-44E4-A2D8-5551E6D3AE27%7D&file=MAST5112MM.docx&action=default&mobileredirect=true)
-
-// <!--Code Attribution-->
-// <!--Yusra, A. 2024.
-// This code has been adapted from classwrok taught by Yusra.
-// Please see the above refrence for the MAST Module Manual to find adapted source code-->
-
 export interface MenuItem {
     id: string;
     name: string;
@@ -23,4 +11,14 @@ export type RootStackParamList = {
     Staff: undefined;
 };
 
+// Code Attribution-->
+// TITLE: MAST5112/d/p/w MODULE MANUAL/GUIDE 2024-->
+// Author: The Independent Institute of Education (Pty) Ltd 2024-->
+// Date: 2024 -->
+// Version: (First Edition: 2012)-->
+// Avialabe:(https://advtechonline.sharepoint.com/:w:/r/sites/TertiaryStudents/_layouts/15/Doc.aspx?sourcedoc=%7B8B4938D0-6B6B-44E4-A2D8-5551E6D3AE27%7D&file=MAST5112MM.docx&action=default&mobileredirect=true)
 
+// <!--Code Attribution-->
+// <!--Yusra, A. 2024.
+// This code has been adapted from classwrok taught by Yusra.
+// Please see the above refrence for the MAST Module Manual to find adapted source code-->
